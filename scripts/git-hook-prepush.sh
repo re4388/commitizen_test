b3890ec0b6b1c0c9977c5ede7bb89e230e1f4a68
re4388 <re4388@gmail.com>
2020-07-20 21:43:31 +0800

    dd

---
+++ scripts/git-hook-prepush.sh
@@ -0,0 +1,18 @@
+echo "[pre-push] Start prepush check"
+# Check for eslint
+echo "[pre-push] Check for NPM..."
+which npm &> /dev/null
+if [[ "$?" == 1 ]]; then
+    echo "Please install NPM"
+    exit 1
+fi
+echo "Pass for checking NPM"
+# Eslint check all files under src/
+echo "Eslint check all files under src/..."
+npm run lint
+if [[ "$?" == 1 ]]; then
+    echo "ESlint check fail, abort git commit"
+    exit 1
+fi
+echo "Pass for Eslint check"
+echo "[pre-commit] successfully"
