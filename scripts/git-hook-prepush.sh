78ae035c4f85d0e9be3f6c1bea689bd35a5d2091
re4388 <re4388@gmail.com>
2020-07-20 21:47:15 +0800

    ci: change pre-commit to pre-push

--- scripts/git-hook-prepush.sh
+++ scripts/git-hook-prepush.sh
@@ -11,8 +11,8 @@ echo "Pass for checking NPM"
 echo "Eslint check all files under src/..."
 npm run lint
 if [[ "$?" == 1 ]]; then
-    echo "ESlint check fail, abort git commit"
+    echo "ESlint check fail, abort git push"
     exit 1
 fi
 echo "Pass for Eslint check"
-echo "[pre-commit] successfully"
+echo "[pre-push] successfully"
